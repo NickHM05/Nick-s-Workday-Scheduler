2dace73c33d699c2768601442943c03fe9bfa363
NicholasHM05 <nhmamberger@gmail.com>
2022-11-20 13:52:49 -0600

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
 
-// handle displaying the time
+// handle displaying the time through moment.js
 function displayTime() {
   var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
   timeDisplayEl.text(rightNow);
